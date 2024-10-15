e776723d56f153bb3b6df5af93b3474ec5fc9802
julia-riley <julia.riley87@gmail.com>
2024-10-15 08:59:39 -0300

    Updating and creating new function for plotting predictions.

---
+++ functions/bci_predictions.R
@@ -0,0 +1,5 @@
+# Body Condition Index - Ordinary Least Squares (OLS) , Scaled Mass Index (SMI), or Both
+
+# This function calculates body condition indices using the ordinary least squares (OLS) or scaled mass index (SMI) methods
+
+# It was partially inspired by Peig & Green (2009) and was adapted from code by Chen-Pan Liao (https://apansharing.blogspot.com/2018/05/an-r-function-olsrobust-caled-mass-index.html)
