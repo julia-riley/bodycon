1d7a7f14b188d06f6c4691550c3190ffa5d9c8a7
julia-riley <julia.riley87@gmail.com>
2024-12-20 11:05:42 -0500

    Changes to both functions to include OLS residual index for body condition
    After reviewing Peig and Green, and also the code from the blog, I have revised both functions. I hope they still work! Let me know what you think. If you want a meeting, then I am happy to do that before 19 Jan.

--- functions/bci_ols_smi.R
+++ functions/bci_ols_smi.R
@@ -5,7 +5,7 @@
 # It was partially inspired by Peig & Green (2009) and was adapted from code by Chen-Pan Liao (https://apansharing.blogspot.com/2018/05/an-r-function-olsrobust-caled-mass-index.html)
 
 
-bci <- function(body_size, weight, method, print) { #Fonti, should we put in a data argument here?
+bci <- function(body_size, weight, bc_index, est_method, print) { #Fonti, should we put in a data argument here so a user could refer to a dataset?
     
   require(smatr)
   require(magrittr)
@@ -15,96 +15,106 @@ bci <- function(body_size, weight, method, print) { #Fonti, should we put in a d
   
   # fonti, this is a note from the paper on pg. 1888:
   
-  # Although any body length value could be used as L0, we recommend those values which falls in the middle range of L (e.g. arithmetic mean, geometric mean, or median), since confidence intervals tend to be narrower in the middle of
+  # Although any body length value could be used as x0, we recommend those values which falls in the middle range of L (e.g. arithmetic mean, geometric mean, or median), since confidence intervals tend to be narrower in the middle of
   # the fitted lnMlnL line than at its extremes.
   
-  #should we include an argument where the reader can decide whether this is mean/median, etc?
+  # Should we include an argument where the reader can decide whether this is mean/median, etc?
   
     
-  }
+}
 
-if(method == "OLS") {
 
-  log_ols <- lm(log(weight) ~ log(body_size))
+if(bc_index == "OLS_residuals") { #for this index, there is not a est_method argument. Is there a way to say that the user should not define that if they select this method?
+  
+  # Fonti, I added this in from lines 29-48 after I realised that I was not calculating the body condition inex based on OLS regression only before... Whoops
+  # I also edited below to adjust object and argument names accordingly with this change :) 
+  
+  ols_regress <- lm(log(weight) ~ log(body_size)) 
+  bci_ols_resid <- resid(ols_regress)
+  
+}
+
+if(print == "index") {
+  
+  bci_ols_resid_only <- data.frame(bci_ols_resid)
+  return(bci_ols_resid_only)
+  
+}
+
+if(print == "index and raw") {
+  bci_ols_resid_and_raw <- data.frame(bci_ols_resid, body_size, weight)
+  return(bci_ols_resid_and_raw)
+  
+}
+  
+}
+
+if(bc_index == "scaled_mass_index" & est_method == "OLS") { #this is the method described in Peig and Green
+
+  log_smi_ols <- lm(log(weight) ~ log(body_size))
   b_msa_ols <- coef(sma(log(weight) ~ log(body_size)))[2]  
-  bci_ols <- weight * (x0 / body_size)^b_msa_ols 
-  
-  # Fonti, to me this may not be the simple OLS method, because I am not sure why the X0 (mean bosy size of the population) is included in this calculation
-  # This is how I think it should be calculated. What do you think?
-  
-  #ols_reg <- lm(log(weight) ~ log(body_size))
-  #bci_ols <- resid(ols_reg)
-  
-  # If this is the case, then this needs to be fixed in the graph function too.
-  # I am now VERY confused about the difference between the OLS method (what I understand to be described in Peig and Green, and the Robust method below - where did it come from)
-  
-  # Okay, I looked up the function "rlm" below and it is a linear model fitted by "robust regression using an M estimator".
-  # So, in short, BOTH of these are the scaled mass index for body condition - one is fit using a standard linear model and the second is fit using a robust linear model.
-  # DARN IT. I should have caught that.
-  
-  #What I would like to do is have (1) the proper OLS residual index (coded in the commented out section above), (2) scaled mass index that is estimated with A. OLS or B. Robust regression (helpful link for this: https://www.spsanderson.com/steveondata/posts/2023-11-28/index.html).
-  # If you do check out the website above, Liao's graphs seems to suggest that the latter does better for some data. 
-  
-  # If you want to have a meeting about this... let me know.
+  bci_smi_ols <- weight * (x0 / body_size)^b_msa_ols 
 
   }
 
   if(print == "index") {
 
-  res_bci_ols_only <- data.frame(bci_ols)
-  return(res_bci_ols_only)
+  bci_smi_ols_only <- data.frame(bci_smi_ols)
+  return(bci_smi_ols_only )
   
   }
 
   if(print == "index and raw") {
-  res_bci_ols_and_raw <- data.frame(bci_ols, body_size, weight)
-  return(res_bci_ols)
+  bci_smi_ols_and_raw <- data.frame(bci_smi_ols, body_size, weight)
+  return(bci_smi_ols_and_raw)
   
   }
   
-if(method == "SMI") {
+if(bc_index == "scaled_mass_index" & est_method == "robust_regression") { #this is a method that was on the blog linked above, and this regression is more robust to outliers
   
   logM_rob <- rlm(log(weight) ~ log(body_size), method = "M")
   b_msa_rob <- coef(sma(log(weight) ~ log(body_size), robust = T))[2]
-  bci_smi <- weight * (x0 / body_size)^b_msa_rob
+  bci_smi_rob <- weight * (x0 / body_size)^b_msa_rob
   
 }
 
 if(print == "index") {
   
-  res_bci_smi_only <- data.frame(bci_smi)
-  return(res_bci_smi_only)
+  bci_smi_rob_only <- data.frame(bci_smi_rob)
+  return(bci_smi_rob_only)
   
 }
 
 if(print == "index and raw") {
   
-  res_bci_smi_and_raw <- data.frame(bci_smi, body_size, weight)
-  return(res_bci_smi_and_raw)
+  bci_smi_rob_and_raw <- data.frame(bci_smi_rob, body_size, weight)
+  return(bci_smi_rob_and_raw)
   
 }
   
-if(method == "both") {  
+if(bc_index == "all" & est_method = "both") {  #fonti, do we have to include the option to calculate bc with ols-residuals, and then smi with ONE of the methods, instead of all? This is a whole bunch of repetition. I am wondering if it is worth it.
   
+    ols_regress <- lm(log(weight) ~ log(body_size)) 
+    bci_ols_resid <- resid(ols_regress)  
     logM_ols <- lm(log(weight) ~ log(body_size))
     logM_rob <- rlm(log(weight) ~ log(body_size), method = "M")
     b_msa_ols <- coef(sma(log(weight) ~ log(body_size)))[2]
     b_msa_rob <- coef(sma(log(weight) ~ log(body_size), robust = T))[2]
-    bci_ols <- weight * (x0 / body_size)^b_msa_ols
-    bci_smi <- weight * (x0 / body_size)^b_msa_rob
+    bci_smi_ols <- weight * (x0 / body_size)^b_msa_ols
+    bci_smi_rob <- weight * (x0 / body_size)^b_msa_rob
     
 }
 
 if(print == "index") {
   
-  res_bci_only <- data.frame(bci_ols, bci_smi)
+  res_bci_only <- data.frame(bci_ols_resid, bci_smi_ols, bci_smi_rob)
   return(res_bci_only)
   
 }
 
 if(print == "index and raw") {
   
-  res_bci_and_raw <- data.frame(bci_ols, bci_smi, body_size, weight)
+  res_bci_and_raw <- data.frame(bci_ols_resid, bci_smi_ols, bci_smi_rob, body_size, weight)
   return(res_bci_and_raw)
   
 }  
--- functions/bci_plot.R
+++ functions/bci_plot.R
@@ -1,15 +1,18 @@
-# PLOT of Body Condition Index Pedictions - Ordinary Least Squares (OLS) , Scaled Mass Index (SMI), or Both
+# PLOT of Body Condition Index Predictions - Ordinary Least Squares (OLS) , Scaled Mass Index (SMI), or Both
 
 # This function is to plot the BCI predictions, so that there can be comparisons between each method or to see how the method you choose visually fits
 
+# It was partially inspired by Peig & Green (2009) and was adapted from code by Chen-Pan Liao (https://apansharing.blogspot.com/2018/05/an-r-function-olsrobust-caled-mass-index.html)
 
-plot_bci <- function(body_size, weight) { #should I put in a data argument here?
+
+
+plot_bci <- function(body_size, weight) { #font, should I put in a data argument here so that people could refer to dataset rather than vectors?
   
     require(smatr)
     require(magrittr)
     require(MASS)
     require(data.table)
-    x0 = mean(body_size)
+    x0 = mean(body_size) #fonti, same question as per the bci_ols_smi.R file. Should we have a way that people could specify mean or median?
   
 
   # fonti, I don't know how to make this function, basically BCI will allow us to make the predictions
@@ -19,20 +22,24 @@ plot_bci <- function(body_size, weight) { #should I put in a data argument here?
     y <- weight
     x <- body_size
     
+    ols_regress <- lm(log(y) ~ log(x)) 
+    bci_ols_resid <- resid(ols_regress)
+    
     log_ols <- lm(log(y) ~ log(x))
     b_msa_ols <- coef(sma(log(y) ~ log(x)))[2]    
-    bci_ols <- y * (x0 / x)^b.msa.ols
+    bci_smi_ols <- y * (x0 / x)^b.msa.ols
     
     logM_rob <- rlm(log(y) ~ log(x), method = "M")
     b_msa_rob <- coef(sma(log(y) ~ log(x), robust = T))[2]
-    bci_smi <- y * (x0 / x)^b.msa.rob
+    bci_smi_rob <- y * (x0 / x)^b.msa.rob
     
-    bci_data <- data.frame(bci_ols, bci_smi, body_size, weight)
+    bci_data <- data.frame(bci_ols_resid, bci_smi_ols, bci_smi_rob, body_size, weight)
     
     pred <-
       data.table(body_size = seq(min(body_size), max(body_size), length = 100)) %>%
-      .[, y_ols := predict(logM_ols, newdata = .) %>% exp()] %>%
-      .[, y_rob := predict(logM_rob, newdata = .) %>% exp()]
+      .[, y_ols_resid := predict(ols_regress, newdata = .) %>% exp()] %>%           #fonti, I am not sure this line is correct, but let's see!
+      .[, y_smi_ols := predict(logM_ols, newdata = .) %>% exp()] %>%
+      .[, y_smi_rob := predict(logM_rob, newdata = .) %>% exp()]
     
     attr(bci_data, "b_msa") <- c(ols = b_msa_ols, smi = b_msa_rob)
     
@@ -54,8 +61,9 @@ plot_bci <- function(body_size, weight) { #should I put in a data argument here?
         x = "Body Size", #Fonti, we have default ones, but also can this be customized by in the argument too? How do we do that?
         y = "Weight"
       ) +
-      geom_line(data = SMI_predictions, aes(body_size, bci_smi), colour = "gray20", size = 1.8) + # Is there a way to add a legend with this line labelled OLS Method
-      geom_line(data = SMI_predictions, aes(body_size, bci_ols), colour = "gold2", linetype = 2, size = 1.8) # This would be labelled SMI Method
+      geom_line(data = pred, aes(body_size, y_ols_resid), colour = "gray20", size = 1.8) + # This would be labelled "Residuals from a OLS Regression"
+      geom_line(data = pred, aes(body_size, y_smi_ols), colour = "darkorange2", linetype = 3, size = 2) + # Is there a way to add a legend with this line labelled "Scaled Mass Index est. with. OLS Regression"
+      geom_line(data = pred, aes(body_size, y_smi_rob), colour = "gold2", linetype = 2, size = 2.2) # This would be labelled "Scaled Mass Index est. with. Robust Regression"
     
     return(SMI_plot)
     
