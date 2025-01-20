5bb62ceed1281a021cc6cfc3cca35ac181e8963f
Fonti Kar <f.kar@unsw.edu.au>
2025-01-21 08:41:11 +1100

    Added smatr as dependency, added na.rm in mean()

--- R/bci_modules.R
+++ R/bci_modules.R
@@ -5,20 +5,22 @@
 # It was partially inspired by Peig & Green (2009) and was adapted from code by Chen-Pan Liao (https://apansharing.blogspot.com/2018/05/an-r-function-olsrobust-caled-mass-index.html)
 
 bci_ols <- function(data, body_size, weight){
-  #browser()
+  # browser()
   
-  x0 = data |> dplyr::pull({{body_size}}) |> mean()
+  # Compute mean of body size
+  x0 = data |> dplyr::pull({{body_size}}) |> mean(na.rm = TRUE)
   
+  # 
   tmp_data <- data |> 
     dplyr::select({{body_size}}, {{weight}}) |> 
-    dplyr::mutate(log_body_size = log({{body_size}}),
-                  log_weight = log({{body_size}}))
+    dplyr::mutate(log_body_size = log({{body_size}}), #TODO: Possible option as an argument log-transform = TRUE as a default 
+                  log_weight = log({{weight}}))
   
   log_ols <- lm(log_weight ~ log_body_size, data = tmp_data)
-  b_msa_ols <- coef(smatr::ma(log_weight ~ log_body_size, data = tmp_data))[2]   # Warning message: In slope.test(Y, X, test.value = NA, method = method, alpha = alpha,  :Group found with zero error variance.
-  bci_ols <- tmp_data |> 
+  b_msa_ols <- coef(smatr::sma(log_weight ~ log_body_size, data = tmp_data))[2]   # Warning message: In slope.test(Y, X, test.value = NA, method = method, alpha = alpha,  :Group found with zero error variance.
+  bci_sma_ols <- tmp_data |> 
     dplyr::mutate(bci_ols = {{weight}} * (x0 / {{body_size}})^b_msa_ols)
   
-  return(bci_ols)
+  return(bci_sma_ols$bci_ols)
 }
 
--- R/bci_ols_smi.R
+++ R/bci_ols_smi.R
@@ -6,19 +6,19 @@
 # 
 # 
 # bci <- function(body_size, weight, method, print) { #should I put in a data argument here?
-#     
+# 
 #   require(smatr)
 #   require(magrittr)
 #   require(MASS)
 #   require(data.table)
 #   x0 = mean(body_size)
-#     
+# 
 #   }
 # 
 # if(method == "OLS") {
 # 
 #   log_ols <- lm(log(weight) ~ log(body_size))
-#   b_msa_ols <- coef(sma(log(weight) ~ log(body_size)))[2]  
+#   b_msa_ols <- coef(sma(log(weight) ~ log(body_size)))[2]
 #   bci_ols <- weight * (x0 / body_size)^b_msa_ols
 # 
 #   }
@@ -27,62 +27,62 @@
 # 
 #   res_bci_ols_only <- data.frame(bci_ols)
 #   return(res_bci_ols_only)
-#   
+# 
 #   }
 # 
 #   if(print == "index and raw") {
 #   res_bci_ols_and_raw <- data.frame(bci_ols, body_size, weight)
 #   return(res_bci_ols)
-#   
+# 
 #   }
-#   
+# 
 # if(method == "SMI") {
-#   
+# 
 #   logM_rob <- rlm(log(weight) ~ log(body_size), method = "M")
 #   b_msa_rob <- coef(sma(log(weight) ~ log(body_size), robust = T))[2]
 #   bci_smi <- weight * (x0 / body_size)^b_msa_rob
-#   
+# 
 # }
 # 
 # if(print == "index") {
-#   
+# 
 #   res_bci_smi_only <- data.frame(bci_smi)
 #   return(res_bci_smi_only)
-#   
+# 
 # }
 # 
 # if(print == "index and raw") {
-#   
+# 
 #   res_bci_smi_and_raw <- data.frame(bci_smi, body_size, weight)
 #   return(res_bci_smi_and_raw)
-#   
+# 
 # }
-#   
-# if(method == "both") {  
-#   
+# 
+# if(method == "both") {
+# 
 #     logM_ols <- lm(log(weight) ~ log(body_size))
 #     logM_rob <- rlm(log(weight) ~ log(body_size), method = "M")
 #     b_msa_ols <- coef(sma(log(weight) ~ log(body_size)))[2]
 #     b_msa_rob <- coef(sma(log(weight) ~ log(body_size), robust = T))[2]
 #     bci_ols <- weight * (x0 / body_size)^b_msa_ols
 #     bci_smi <- weight * (x0 / body_size)^b_msa_rob
-#     
+# 
 # }
 # 
 # if(print == "index") {
-#   
+# 
 #   res_bci_only <- data.frame(bci_ols, bci_smi)
 #   return(res_bci_only)
-#   
+# 
 # }
 # 
 # if(print == "index and raw") {
-#   
+# 
 #   res_bci_and_raw <- data.frame(bci_ols, bci_smi, body_size, weight)
 #   return(res_bci_and_raw)
-#   
-# }  
-#     
-#     
-#     
+# 
+# }
+# 
+# 
+# 
 # 
