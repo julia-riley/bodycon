4333dac89acf1480be414fd66bc71b48de350fca
julia-riley <julia.riley87@gmail.com>
2025-02-24 13:50:56 -0400

    Added Robust Regression function and references for it

--- R/bci_modules.R
+++ R/bci_modules.R
@@ -52,7 +52,7 @@ bci_resid_ols <- function(data, body_size, weight){
 
 #' Scaled Mass Body Condition Index Estimation with OLS Regression
 #' @description 
-#' This function calculates body condition indices using the scaled mass index (SMI method) as described in \insertCite{peig2009;textual}{bodycon}. Specifically, this methods uses ordinary least squares regression in its estimation of the body condition indices. Yet, this method is sensitive to the presence of outliers (i.e., data points that may distort the expected relationship between body length and weight), and so SMI estimation using robust regression (see function `name`) may be more appropriate in cases where outliers are present.
+#' This function calculates body condition indices using the scaled mass index (SMI method) as described in \insertCite{peig2009;textual}{bodycon}. Specifically, this methods uses ordinary least squares regression in its estimation of the body condition indices. Yet, this method is sensitive to the presence of outliers (i.e., data points that may distort the expected relationship between body length and weight), and so SMI estimation using robust regression (see function `bci_smi_rob`) may be more appropriate in cases where outliers are present.
 
 #' #NOTE: FONTI, can we refer to another function in this text? Also, I am not sure if I used the correct notation for a citation; trying something to figure out the references here.
 
@@ -102,4 +102,62 @@ bci_smi_ols <- function(data, body_size, weight){
   
   return(bci$bci_smi_ols)
 }
+
+
+
+#' Scaled Mass Body Condition Index Estimation with Robust Regression
+#' @description 
+#' This function calculates body condition indices using the scaled mass index (SMI method) as described in \insertCite{peig2009;textual}{bodycon}. Specifically, this methods uses robust regression using an M estimator (from the MASS R package) \insertCite{venables2002}{bodycon} in its estimation of the body condition indices. This method is less sensitive to the presence of outliers (i.e., data points that may distort the expected relationship between body length and weight), as shown in [this blog by by Chen-Pan Liao](https://apansharing.blogspot.com/2018/05/an-r-function-olsrobust-caled-mass-index.html).
+
+#' #Note to Fonti: Can we refer to a link in this text? Also, I am not sure if I used the correct notation for a citation; trying something to figure out the references here.
+
+#' @param data tibble/dataframe containing a standard body size variable and the corresponding weight for each individual of one animal species
+#' @param body_size name of standard body size variable (e.g., snout-vent-length of reptiles, tarsus length of birds, length from the snout to the base of the tail for mammals, etc.)
+#' @param weight name of weight variable (e.g., mass of the animal)
+#' 
+#' @return a vector of body condition indices for each individual estimates using the SMI method using a robust regression
+#' @importFrom Rdpack reprompt
+#' 
+#' @references 
+#'   \references{
+#'      \insertAllCited{}
+#'      }
+#' 
+#' @examples 
+#' # In this examples we will make use of the Hawks dataset in the Stat2Data R package
+#' # This dataset contains the weight (in grams) and tarsus length (in mm) of three North American Hawk species
+#' # To estimate body condition indices (using the scaled mass index with a robust regression) for the Red-tailed Hawks from this dataset, one could:
+#' 
+#' # library(Stat2Data)
+#' # library(bodycon)
+#' # data(Hawks) 
+#' # Hawks |>
+#' #   filter(species == "RT") |>
+#' #   bci_smi_rob(Tarsus, Weight)
+#' 
+#' # Note to Fonti: The above is a potential example? We could write it a different way. If we add additional arguments, then we would also have to add examples with those. This example also does not really include outliers? Does that matter?
+#' 
+bci_smi_rob <- function(data, body_size, weight){
+  #browser()
+  
+  # Compute mean of body size
+  x0 = data |> dplyr::pull({{body_size}}) |> mean(na.rm = TRUE)
+  
+  # Create tmp data for log transformations
+  tmp_data <- data |> 
+    dplyr::select({{body_size}}, {{weight}}) |> 
+    dplyr::mutate(log_body_size = log({{body_size}}), #Note here about log-transformation: for the scale-mass index calculation of body condition, log-transformation needs to occur, so it cannot be an option for this module 
+                  log_weight = log({{weight}}))
+  
+  # Compute OLS
+  log_rob <- MASS::rlm(log_weight ~ log_body_size, method = "M", data = tmp_data)
+  b_msa_rob <- coef(smatr::sma(log_weight ~ log_body_size, robust = T, data = tmp_data))[2]   
+  bci <- tmp_data |> 
+    dplyr::mutate(bci_smi_rob = {{weight}} * (x0 / {{body_size}})^b_msa_rob)
+  
+  return(bci$bci_smi_rob)
+}
+
+
+
 
