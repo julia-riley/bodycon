9fac05809d44d3ee8863683c841d966d213bc6dd
Fonti Kar <fonti.kar@gmail.com>
2025-06-18 07:47:33 +1000

    Added workflow to add data for JR

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Hawks Data from Stat2Data (placeholder)
+#'
+#' A subset of data from the World Health Organization Global Tuberculosis
+#' Report ...
+#'
+#' @format ## `who`
+#' A data frame with 7,240 rows and 60 columns:
+#' \describe{
+#'   \item{var1}{Country name}
+#'   \item{var2}{2 & 3 letter ISO country codes}
+#'   \item{var3}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"hawks"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,20 @@
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+# #Dependencies
+library(Stat2Data)
+# Example: Hawks (placeholder) 
+data("Hawks")
+hawks <- Hawks
+
+# # Code to made the data for R package
+# # Snake
+# data <- read_csv(...
+# )
+# 
+# output <- data  |> filter() |> rename()
+# 
+# # Sallie data
+
+# https://r-pkgs.org/data.html#sec-documenting-data
+
+usethis::use_data(hawks, overwrite = TRUE)
