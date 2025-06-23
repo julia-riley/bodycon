3f944f652a86487e3b04ad4f85ce8f00a81aff83
julia-riley <julia.riley87@gmail.com>
2025-06-23 13:35:58 -0400

    Dataset creation and description
    Created two datasets - one on snakes and one on salamanders. I also included the descriptions for these datasets.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,20 +1,41 @@
-## code to prepare `DATASET` dataset goes here
+## Code to prepare the datasets used in the bodycon R package
 
-# #Dependencies
-library(Stat2Data)
-# Example: Hawks (placeholder) 
-data("Hawks")
-hawks <- Hawks
+library(tidyverse)
 
-# # Code to made the data for R package
-# # Snake
-# data <- read_csv(...
-# )
-# 
-# output <- data  |> filter() |> rename()
-# 
-# # Sallie data
+# Snake Data 
+snake_data <- read_csv("data-raw/issac acker_snake_data.csv")
 
-# https://r-pkgs.org/data.html#sec-documenting-data
+glimpse(snake_data)
+table(snake_data$species)
 
-usethis::use_data(hawks, overwrite = TRUE)
+gartersnake_data <- 
+  snake_data |>
+  select("species", "snake_id_no", "age (A/J)", "sex (M/F)", "mass_g", "svl_mm", "total_length_mm") |>
+  filter(species == "Maritime Garter",
+         !is.na(svl_mm),
+         !is.na(mass_g)) |>
+  select("snake_id_no", "age (A/J)", "sex (M/F)", "mass_g", "svl_mm", "total_length_mm") |>
+  rename("age_class" = `age (A/J)`, 
+         "sex" = `sex (M/F)`,
+         "id_num" = snake_id_no)
+
+
+# Salamander Data 
+salamander_data <- read_csv("data-raw/sara leslie_salamander_data.csv")
+
+glimpse(salamander_data)
+table(snake_data$species)
+
+erbs_data <- 
+  salamander_data |>
+  select("species", "group", "salamander_ID", "sex", "morph", "gravid", "age", "mass_g", "svl_mm", "total_length_mm") |>
+  filter(species == "RB",
+         group == "control",
+         gravid == "n" | NA,
+         !is.na(svl_mm),
+         !is.na(mass_g)) |>
+  select("salamander_ID", "sex", "morph", "gravid", "age", "mass_g", "svl_mm", "total_length_mm")
+
+dim(erbs_data)
+
+usethis::use_data(gartersnake_data, erbs_data, overwrite = TRUE)
