b7024d1cbe238a30a8dbeb5787c4e987318a873a
Fonti Kar <f.kar@unsw.edu.au>
2025-06-24 07:32:57 +1000

    Apply suggestions from code review
    Adding namespaces, giving datasets a one word name

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,6 @@
 ## Code to prepare the datasets used in the bodycon R package
 
-library(tidyverse)
+library(dplyr)
 
 # Snake Data 
 snake_data <- read_csv("data-raw/issac acker_snake_data.csv")
@@ -8,14 +8,14 @@ snake_data <- read_csv("data-raw/issac acker_snake_data.csv")
 glimpse(snake_data)
 table(snake_data$species)
 
-gartersnake_data <- 
+gartersnake <- 
   snake_data |>
-  select("species", "snake_id_no", "age (A/J)", "sex (M/F)", "mass_g", "svl_mm", "total_length_mm") |>
-  filter(species == "Maritime Garter",
+  dplyr::select("species", "snake_id_no", "age (A/J)", "sex (M/F)", "mass_g", "svl_mm", "total_length_mm") |>
+  dplyr::filter(species == "Maritime Garter",
          !is.na(svl_mm),
          !is.na(mass_g)) |>
-  select("snake_id_no", "age (A/J)", "sex (M/F)", "mass_g", "svl_mm", "total_length_mm") |>
-  rename("age_class" = `age (A/J)`, 
+    dplyr::select("snake_id_no", "age (A/J)", "sex (M/F)", "mass_g", "svl_mm", "total_length_mm") |>
+    dplyr::rename("age_class" = `age (A/J)`, 
          "sex" = `sex (M/F)`,
          "id_num" = snake_id_no)
 
@@ -23,19 +23,17 @@ gartersnake_data <-
 # Salamander Data 
 salamander_data <- read_csv("data-raw/sara leslie_salamander_data.csv")
 
-glimpse(salamander_data)
-table(snake_data$species)
 
-erbs_data <- 
+salamander <- 
   salamander_data |>
-  select("species", "group", "salamander_ID", "sex", "morph", "gravid", "age", "mass_g", "svl_mm", "total_length_mm") |>
-  filter(species == "RB",
+    dplyr::select("species", "group", "salamander_ID", "sex", "morph", "gravid", "age", "mass_g", "svl_mm", "total_length_mm") |>
+    dplyr::filter(species == "RB",
          group == "control",
          gravid == "n" | NA,
          !is.na(svl_mm),
          !is.na(mass_g)) |>
-  select("salamander_ID", "sex", "morph", "gravid", "age", "mass_g", "svl_mm", "total_length_mm")
+    dplyr::select("salamander_ID", "sex", "morph", "gravid", "age", "mass_g", "svl_mm", "total_length_mm")
 
 dim(erbs_data)
 
-usethis::use_data(gartersnake_data, erbs_data, overwrite = TRUE)
+usethis::use_data(gartersnake, salamander, overwrite = TRUE)
