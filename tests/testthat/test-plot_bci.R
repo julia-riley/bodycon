8f4b646a506058c3b5f7cbfb12d5c90562ff62d3
Fonti Kar <fonti.kar@gmail.com>
2026-01-30 09:27:33 +1100

    Added testing infrastructure #19

---
+++ tests/testthat/test-plot_bci.R
@@ -0,0 +1,39 @@
+test_that("multiplication works", {
+  # TODO: What are the expected output for these
+  # TODO: Need vdiffr expect_dopplerganger
+  
+  plot_bci(
+    gartersnake_data,
+    svl_mm,
+    mass_g,
+    method = c("smi_ols"),
+    legend = FALSE
+  )
+  
+  
+  plot_bci(
+    gartersnake_data,
+    svl_mm,
+    mass_g,
+    method = c("smi_ols", "smi_rob"),
+    group = "sex",
+    raw_colours = c("M" = "darkorange2", "F" = "mediumpurple"),
+    method_colours = c("OLS residuals" = "black", "SMI (robust)" = "navy")
+  )
+  
+  plot_bci(
+    gartersnake_data,
+    svl_mm,
+    mass_g,
+    method = c("smi_rob"),
+    group = "sex"
+  )
+  
+  plot_bci(
+    gartersnake_data,
+    svl_mm,
+    mass_g,
+    method = c("resid_ols", "smi_ols", "smi_rob")
+  )
+  
+})
