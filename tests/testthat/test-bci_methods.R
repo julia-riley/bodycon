fddc6e33cd5761e12071f4fe50e37a2a2221b1f3
julia-riley <julia.riley87@gmail.com>
2026-06-05 16:52:09 -0300

    Trialling tests for the bci estimation functions.
    These coudl be further refined because I am not sure what the end-goal is. But, they are looking like they are calculating the correct output values.

--- tests/testthat/test-bci_methods.R
+++ tests/testthat/test-bci_methods.R
@@ -1,16 +1,48 @@
 test_that("Check output values are correct", {
-  gartersnake |>
-    bci_resid_ols(svl_mm, mass_g)
   
-  gartersnake_data |>
-    bci_smi_ols(svl_mm, mass_g)
+  # OLS Residuals
+  ## Using the R package
+  bci_resid_ols_pkg <- gartersnake |>
+                        bci_resid_ols(svl_mm, mass_g)
+
+  ## Estimation by hand 
+  ols <- lm(log(mass_g) ~ log(svl_mm), data = gartersnake)
+  bci_resid_ols_hand <- ols$residuals
+ 
+  ## Is bci_resid_ols calculating what we expect (values)?  
+  identical(bci_resid_ols_pkg, bci_resid_ols_hand)
   
-  gartersnake_data |>
-    bci_smi_rob(svl_mm, mass_g)
+  # SMI using OLS regression
+  ## Using the R package
+  bci_smi_ols_pkg <- gartersnake |>
+                    bci_smi_ols(svl_mm, mass_g)
   
+  ## Estimation by hand 
+  x0 <- mean(gartersnake$svl_mm)
+  logM_ols <- lm(log(gartersnake$mass_g) ~ log(gartersnake$svl_mm))
+  b_msa_ols <- coef(smatr::sma(log(gartersnake$mass_g) ~ log(gartersnake$svl_mm)))[2]
+  SMI_ols <- gartersnake$mass_g * (x0 / gartersnake$svl_mm)^b_msa_ols
+  bci_smi_ols_hand <- SMI_ols
+  
+  ## Is bci_smi_ols calculating what we expect (values)?  
+  identical(bci_smi_ols_pkg, bci_smi_ols_hand)
+  
+  # SMI using robust regression
+  ## Using the R package
+  bci_smi_rob_pkg <- gartersnake_data |>
+                        bci_smi_rob(svl_mm, mass_g)
+  
+  ## Estimation by hand 
+  x0 <- mean(gartersnake$svl_mm)
+  logM_rob <- MASS::rlm(log(gartersnake$mass_g) ~ log(gartersnake$svl_mm), method = "M")
+  b_msa_rob <- coef(smatr::sma(log(gartersnake$mass_g) ~ log(gartersnake$svl_mm), robust = T))[2]
+  SMI_rob <- gartersnake$mass_g * (x0 / gartersnake$svl_mm)^b_msa_rob
+  bci_smi_rob_hand <- SMI_rob
+
+  ## Is bci_smi_rob calculating what we expect (values)?  
+  identical(bci_smi_rob_pkg, bci_smi_rob_hand)
+    
 })
-
-
 
 
 
--- tests/testthat/test-plot_bci.R
+++ tests/testthat/test-plot_bci.R
@@ -1,5 +1,6 @@
 test_that("Plotting works", {
-  # TODO: What are the expected output for these
+  # TODO: What are the expected output for these; JLR: A plot
+  # TODO, JLR: The lines are incorrect. So, I need to fix these.
   # TODO: Need vdiffr expect_dopplerganger
   
   plot_bci(
