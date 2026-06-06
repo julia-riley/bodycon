b6d2c90b1f44dedc68d6e09746e351b1dee6df21
julia-riley <julia.riley87@gmail.com>
2026-06-06 12:49:13 -0300

    Created test of plotting function using vdiff

--- tests/testthat/test-plot_bci.R
+++ tests/testthat/test-plot_bci.R
@@ -1,38 +1,116 @@
-test_that("Plotting works", {
-  # TODO: Need vdiffr expect_dopplerganger
+# Test of Basic Plot
+test_that("plot_bci basic plot renders correctly", {
   
-  plot_bci(
-    gartersnake_data,
-    svl_mm,
-    mass_g,
-    method = c("smi_ols"),
+  p <- plot_bci(
+    data = gartersnake,
+    body_size = svl_mm,
+    weight = mass_g,
+    legend = TRUE
+  )
+  
+  vdiffr::expect_doppelganger("bci-basic", p)
+})
+
+
+# Tests of Plot with Grouping
+## Age
+test_that("plot_bci with grouping renders correctly", {
+  
+  p <- plot_bci(
+    data = salamander,
+    body_size = svl_mm,
+    weight = mass_g,
+    group = age,
+    legend = TRUE
+  )
+  
+  vdiffr::expect_doppelganger("bci-group-age", p)
+})
+
+## Sex
+test_that("plot_bci with grouping renders correctly", {
+  
+  p <- plot_bci(
+    data = gartersnake,
+    body_size = svl_mm,
+    weight = mass_g,
+    group = sex,
+    legend = TRUE
+  )
+  
+  vdiffr::expect_doppelganger("bci-group-sex", p)
+})
+
+
+# OLS residual method
+test_that("plot_bci only OLS method", {
+  
+  p <- plot_bci(
+    data = salamander,
+    body_size = svl_mm,
+    weight = mass_g,
+    method = "resid_ols"
+  )
+  
+  vdiffr::expect_doppelganger("bci-ols-only", p)
+})
+
+
+# SMI OLS method only
+test_that("plot_bci SMI OLS only", {
+  
+  p <- plot_bci(
+    data = salamander,
+    body_size = svl_mm,
+    weight = mass_g,
+    method = "smi_ols"
+  )
+  
+  vdiffr::expect_doppelganger("bci-smi-ols-only", p)
+})
+
+
+# SMI robus method only
+test_that("plot_bci SMI robust only", {
+  
+  p <- plot_bci(
+    data = gartersnake,
+    body_size = svl_mm,
+    weight = mass_g,
+    method = "smi_rob"
+  )
+  
+  vdiffr::expect_doppelganger("bci-smi-rob-only", p)
+})
+
+
+# Plot without legend
+test_that("plot_bci without legend", {
+  
+  p <- plot_bci(
+    data = salamander,
+    body_size = svl_mm,
+    weight = mass_g,
+    group = sex,
     legend = FALSE
   )
   
+  vdiffr::expect_doppelganger("bci-no-legend", p)
+})
+
+
+# Plot tha also returns predictions
+test_that("plot_bci returns predictions correctly", {
   
-  plot_bci(
-    gartersnake_data,
-    svl_mm,
-    mass_g,
-    method = c("smi_ols", "smi_rob"),
-    group = "sex",
-    raw_colours = c("M" = "darkorange2", "F" = "mediumpurple"),
-    method_colours = c("OLS residuals" = "black", "SMI (robust)" = "navy")
+  out <- plot_bci(
+    data = salamander,
+    body_size = svl_mm,
+    weight = mass_g,
+    return_predictions = TRUE
   )
   
-  plot_bci(
-    gartersnake_data,
-    svl_mm,
-    mass_g,
-    method = c("smi_rob"),
-    group = "sex"
-  )
-  
-  plot_bci(
-    gartersnake_data,
-    svl_mm,
-    mass_g,
-    method = c("resid_ols", "smi_ols", "smi_rob")
-  )
-  
+  expect_true(is.list(out))
+  expect_true("plot" %in% names(out))
+  expect_true("predictions" %in% names(out))
+  expect_true("data" %in% names(out))
 })
